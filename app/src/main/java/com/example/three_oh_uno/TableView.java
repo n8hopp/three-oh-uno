1532e19de88c1af82d3c3d76b24d76b62e7e1996
n8hopp <112525308+n8hopp@users.noreply.github.com>
2022-09-23 18:24:48 -0700

    TableView updated for cards to be bigger and color to be more appealing

--- app/src/main/java/com/example/three_oh_uno/TableView.java
+++ app/src/main/java/com/example/three_oh_uno/TableView.java
@@ -28,28 +28,37 @@ public class TableView extends SurfaceView {
 		paint1.setARGB(255, 255, 255, 255);
 		paint2.setARGB(255, 255, 0, 0); // Two separate colors to differentiate stacked rectangles if the rectangles are stacked
 												   // or to make text on cards the opposite color
-		tableColor.setARGB(255, 50, 255, 50);
+		tableColor.setARGB(255, 66, 143, 70);
 		tableColor.setStyle(Paint.Style.FILL);
 
 		paint1.setTextAlign(Paint.Align.CENTER);
 		paint2.setTextAlign(Paint.Align.CENTER);
 
-		paint1.setTextSize(20);
+		paint1.setTextSize(45);
 		paint2.setTextSize(paint1.getTextSize());
 
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
+		//Background
 		canvas.drawRect(0, 0, (getWidth()), (getHeight()), tableColor);
-		canvas.drawRect((getWidth()/2)-50, 50, (getWidth()/2)+50, 200, paint1);
-		canvas.drawText("3 cards", (getWidth()/2), 130, paint2);
-		canvas.drawRect(50, (getHeight()/2)-50, 200, (getHeight()/2)+50, paint2);
-		canvas.drawText("4 cards", 125, (getHeight()/2)+5, paint1);
-		canvas.drawRect((getWidth()-200), (getHeight()/2)-50, (getWidth()-50), (getHeight()/2)+50, paint1);
-		canvas.drawText("6 cards", (getWidth()-125), (getHeight()/2)+5, paint2);
-		canvas.drawRect((getWidth()/2)+25,  (getHeight()/2)-75, (getWidth()/2)+125, (getHeight()/2)+75, paint2);
-		canvas.drawRect((getWidth()/2)-125,  (getHeight()/2)-75, (getWidth()/2)-25, (getHeight()/2)+75, paint1);
+
+		// Top card
+		canvas.drawRect((getWidth()/2)-100, 50, (getWidth()/2)+100, 350, paint1);
+		canvas.drawText("3 cards", (getWidth()/2), 200, paint2);
+
+		// Left card
+		canvas.drawRect(50, (getHeight()/2)-100, 350, (getHeight()/2)+100, paint2);
+		canvas.drawText("4 cards", 200, (getHeight()/2)+5, paint1);
+
+		// Right card
+		canvas.drawRect((getWidth()-350), (getHeight()/2)-100, (getWidth()-50), (getHeight()/2)+100, paint1);
+		canvas.drawText("6 cards", (getWidth()-200), (getHeight()/2)+5, paint2);
+
+		// Middle cards
+		canvas.drawRect((getWidth()/2)+25,  (getHeight()/2)-150, (getWidth()/2)+225, (getHeight()/2)+150, paint2);
+		canvas.drawRect((getWidth()/2)-225,  (getHeight()/2)-150, (getWidth()/2)-25, (getHeight()/2)+150, paint1);
 
 		/* Nate:
 			This is all godawful. Please make this dynamic ASAP. I cannot deal with manually
